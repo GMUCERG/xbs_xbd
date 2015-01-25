0b543001eadfcda35dccf092f8df797b3ae5f668
John Pham <jhnphm@gmail.com>
2015-01-25 16:53:15 -0500

    Changed HAL to use linker symbols for stack measurement
    Assembly instructions used to get top of stack
    Also used tivaware functions to get limits of flash and flash block size
    Using tivaware functions to write to flash

--- platforms/tm4c123gh6pm_80mhz/hal/XBD_DeviceDependent.h
+++ platforms/tm4c123gh6pm_80mhz/hal/XBD_DeviceDependent.h
@@ -1,3 +1,14 @@
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_flash.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+
+
+
 
 /** define XBX_LITTLE_ENDIAN or XBX_BIG_ENDIAN */
 #define XBX_LITTLE_ENDIAN
@@ -9,19 +20,20 @@
 
 /** define size of a page to program, keep at this value if there is no page 
 *   concept */
-#define PAGESIZE 1024
+#define PAGESIZE (MAP_SysCtlFlashSectorSizeGet())
 /** mask for alignment of page boundaries, keep at this value if there is no
 * page concept */
-#define PAGE_ALIGN_MASK 0xfffffc00
+#define PAGE_ALIGN_MASK (~MAP_SysCtlFlashSectorSizeGet()) 
 
 
 /** maximum flash / binary storage size */
-#define FLASH_ADDR_MAX (0xffff)
+#define FLASH_ADDR_MAX (PAGESIZE-0x2000)
 
 /** minimum flash address, if boot loader resides below app */
 #define FLASH_ADDR_MIN (0x2000)
 
 #define DEVICE_SPECIFIC_SANE_TC_VALUE 600000
 
-#define LM3S811_STACKTOP ((uint8_t *)0x20002000)
-#define LM3S811_STACKBOTTOM ((uint8_t *)0x20001c00)
+// Use extern end to find end of .bss instead
+//#define TM4C123GH6PM_STACKTOP ((uint8_t *)0x20002000)
+//#define TM4C123GH6PM_STACKBOTTOM ((uint8_t *)0x20001c00)
