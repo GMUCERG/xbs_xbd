9f1a7c76e6e58c76252f163a05e24d9d614acaa9
John Pham <jhnphm@gmail.com>
2015-03-01 00:48:33 -0500

    MAY BREAK I2C on ATMEGA: Made conditional some slow debug statements
    Disabled debug statements when no DEBUG macro defined. According to comments
    this may affect timing and break I2C.

--- embedded/xbd/xbd_app/XBD_multipacket.c
+++ embedded/xbd/xbd_app/XBD_multipacket.c
@@ -95,7 +95,10 @@ uint32_t XBD_genInitialMultiPacket(const uint8_t* srcdata, uint32_t srclen, uint
 	//XBD_debugOutBuffer("srclen:",&srclen, 4);
 	//XBD_debugOutBuffer("srclen@target:",dstbuf+offset, 4);
 	offset+=LENGSIZE;
+#ifdef DEBUG
 	XBD_debugOut("\noffset="), XBD_debugOutHex32Bit(offset);
+    XBD_debugOut("\n");
+#endif
 	realTXlen=XBD_ANSWERLENG_MAX;
 	return offset;
 }
