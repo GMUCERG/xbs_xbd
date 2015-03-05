cc417335e99af3e1be28d53f44c5e29e321c6a6f
John Pham <jhnphm@gmail.com>
2015-03-05 03:18:48 -0500

    Use compiler intrinisics for byte swaps

--- embedded/xbd/xbd_af/XBD_HAL.h
+++ embedded/xbd/xbd_af/XBD_HAL.h
@@ -8,11 +8,17 @@
 
 
 #ifdef XBX_LITTLE_ENDIAN
-
+#ifdef __GNUC__
+    #define NTOHL(x) __builtin_bswap32(x)
+    #define NTOHS(x) __builtin_bswap16(x)
+    #define HTONL(x) __builtin_bswap32(x)
+    #define HTONS(x) __builtin_bswap16(x)
+#else
 	#define NTOHL(x) ((x & 0xFF000000)>>24)+((x & 0x00FF0000)>>8)+((x & 0x0000FF00)<<8)+((x & 0x000000FF)<<24)
 	#define HTONL(x) ((x & 0xFF000000)>>24)+((x & 0x00FF0000)>>8)+((x & 0x0000FF00)<<8)+((x & 0x000000FF)<<24)
 	#define NTOHS(x) (((x & 0xFF00)>>8)+((x & 0x00FF)<<8))
 	#define HTONS(x) (((x & 0xFF00)>>8)+((x & 0x00FF)<<8))
+#endif
 #else
 	#define NTOHL(x) (x)
 	#define HTONL(x) (x) 
