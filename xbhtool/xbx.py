157bad615e39f0f362bd0e4d400b0bfcf86fe03e
John Pham <jhnphm@gmail.com>
2015-03-13 16:55:51 -0400

    Added checksum reading to Config

--- xbhtool/xbx.py
+++ xbhtool/xbx.py
@@ -21,33 +21,33 @@ class Config:
     algorithms = {}
 
 
-
     def __init__(self, filename):
         config = configparser.ConfigParser()
         config.read(filename)
 
         ## Basic path configuration
-        self.all_platforms_path = config['paths']['platforms']
-        self.algopack_path = config['paths']['algopacks']
-        self.embedded_path = config['paths']['embedded']
-        self.work_path     = config['paths']['work']
+        self.all_platforms_path = config.get('paths','platforms')
+        self.algopack_path      = config.get('paths','algopacks')
+        self.embedded_path      = config.get('paths','embedded')
+        self.work_path          = config.get('paths','work')
 
 
         ## Platform configuration
-        self.platform = config['hardware']['platform']
+        self.platform = config.get('hardware','platform')
         self.platform_path = os.path.join(self.all_platforms_path, self.platform)
         config.read(os.path.join(self.platform_path, "settings.ini"))
         if config.has_option('platform_settings', 'templatePlatform'):
             self.platform_templ_path = os.path.join(
-                    self.all_platforms_path, config['platform_settings']['templatePlatform'])
-        self.clock_hz = config['platform_settings']['cyclespersecond']
-        self.pagesize = config['platform_settings']['pagesize']
+                    self.all_platforms_path, config.get('platform_settings','templatePlatform'))
+
+        self.clock_hz = config.getint('platform_settings','cyclespersecond')
+        self.pagesize = config.getint('platform_settings','pagesize')
 
 
 
         ## Get algorithms and implementations
-        self.operation = config['algorithm']['operation']
-        algorithms = config['algorithm']['algorithms'].split("\n")
+        self.operation = config.get('algorithm','operation')
+        algorithms     = config.get('algorithm','algorithms').split("\n")
 
         # Get operation path
         op_path = os.path.join(self.algopack_path, "crypto_"+self.operation)
@@ -65,14 +65,18 @@ class Config:
             self.algorithms[i].path = os.path.join(op_path,i)
             self.algorithms[i].impls = {}
 
+            checksumfile = os.path.join(self.algorithms[i].path, "checksumsmall")
+            with open(checksumfile) as f:
+                self.algorithms[i].checksumsmall = f.readline().strip()
+
         blacklist = []
         whitelist = []
 
-        if config.has_option('implementation', 'blacklist') and config['implementation']['blacklist']:
-            blacklist = config['implementation']['blacklist'].split("\n")
+        if config.has_option('implementation', 'blacklist') and config.get('implementation','blacklist'):
+            blacklist = config.get('implementation','blacklist').split("\n")
 
-        if config.has_option('implementation', 'whitelist') and config['implementation']['whitelist']:
-            whitelist = config['implementation']['whitelist'].split("\n")
+        if config.has_option('implementation', 'whitelist') and config.get('implementation','whitelist'):
+            whitelist = config.get('implementation','whitelist').split("\n")
 
         # Get whitelist  ^blacklist
         for algorithm, item in self.algorithms.items():
