51e189a01b1dc8ef00842015b6cc50d712620789
John Pham <jhnphm@gmail.com>
2015-03-31 18:12:25 -0400

    Change return value to big endian in checksum test

--- embedded/xbd/xbd_af/XBD_OH_test.c
+++ embedded/xbd/xbd_af/XBD_OH_test.c
@@ -35,10 +35,11 @@ uint8_t OH_handleChecksumRequest(uint8_t *parameterBuffer, uint8_t* resultBuffer
 
     // If try_failed is true, error has occured
     // Currently don't report why failed other than via serial port
-	resultBuffer[0]=try_failed?1:0;
+    //Big endian format, so 0 pad first
+	resultBuffer[0]=0;
 	resultBuffer[1]=0;
 	resultBuffer[2]=0;
-	resultBuffer[3]=0;
+	resultBuffer[3]=try_failed?1:0;
 	
 	/* stop the watchdog */
 	XBD_stopWatchDog();
