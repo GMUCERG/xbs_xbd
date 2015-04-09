ea3723d504a18169c60f8c473819b93d2ff0cb30
John Pham <jhnphm@gmail.com>
2015-04-09 02:26:43 -0400

    Parameter/result buffer sizes now defined by operation

--- embedded/xbd/xbd_app/XBD_APP.h
+++ embedded/xbd/xbd_app/XBD_APP.h
@@ -1,6 +1,14 @@
 #ifndef _XBD_APP_H
 #define _XBD_APP_H
+// Examine XBD_commands.h for other important parameters
+
+// Defined in XBD_commands.h
+// #define XBD_ANSWERLENG_MAX 48
 #include "XBD_commands.h"
-#define XBD_PARAMLENG_MAX (2048+ADDRSIZE)
-#define XBD_RESULTLEN (2048)
+
+
+// Defined in XBD_operation.h
+// #define XBD_PARAMLENG_MAX (2048+ADDRSIZE)
+// #define XBD_RESULTLEN (512)
+#include "XBD_operation.h"
 #endif
--- embedded/xbd/xbd_op/crypto_hash/XBD_operation.h
+++ embedded/xbd/xbd_op/crypto_hash/XBD_operation.h
@@ -1,4 +1,12 @@
-#include <crypto_hash.h>
+#include "crypto_hash.h"
+#include "XBD_commands.h"
+
 #define CRYPTO_HASH_TYPE 0x1
 #define EBASH_TYPE CRYPTO_HASH_TYPE
+
+#define MAX_MSG_LEN 2048
+#define MAX_OUTPUT_LEN 128
+
 #define XBD_OPERATION_TYPE CRYPTO_HASH_TYPE
+#define XBD_PARAMLENG_MAX (MAX_MSG_LEN)
+#define XBD_RESULTLEN (NUMBSIZE+MAX_OUTPUT_LEN)
