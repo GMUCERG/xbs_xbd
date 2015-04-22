75935f4023a1874d8be17242405b205edec2103d
John Pham <jhnphm@gmail.com>
2015-04-22 17:10:57 -0400

    Add null cipher for aead

---
+++ algobase/crypto_aead/0cipher/empty/encrypt.c
@@ -0,0 +1,27 @@
+#include "crypto_aead.h"
+
+
+int crypto_aead_encrypt(
+	unsigned char *c,unsigned long long *clen,
+	const unsigned char *m,unsigned long long mlen,
+	const unsigned char *ad,unsigned long long adlen,
+	const unsigned char *nsec,
+	const unsigned char *npub,
+	const unsigned char *k )
+{
+    *clen = mlen;
+    return 0;
+}
+
+int crypto_aead_decrypt(
+	unsigned char *m,unsigned long long *mlen,
+	unsigned char *nsec,
+	const unsigned char *c,unsigned long long clen,
+	const unsigned char *ad,unsigned long long adlen,
+	const unsigned char *npub,
+	const unsigned char *k)
+{
+    *mlen = clen;
+    return 0;
+}
+
