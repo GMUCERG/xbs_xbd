8ee5cfde9defa3fd5fd487de612bac0212d1bf20
raghuvelagala <raghu@velagala.org>
2018-05-01 05:32:01 -0400

    plot.py updated for plotting multiple algos in DB; Plots stored in /plots dir

--- plot.py
+++ plot.py
@@ -22,60 +22,66 @@ def main():
     logging.config.fileConfig("logging.ini", disable_existing_loggers=False)
     xbxdb.init(xbxu.get_db_path(CONFIG_PATH))
     s = xbxdb.scoped_session()
-    rs = s.query(xbxr.RunSession).order_by(xbxr.RunSession.id.desc()).first()
+    for bx in  s.query(xbxr.BuildExec).order_by(xbxr.BuildExec.id.desc()):
+
+        impl = bx.build.implementation.path
+
+        # q is filtered for everything in last runsession, only encryption results
+        q = (s.query(xbxro.CryptoAeadRun)
+        .join(xbxr.BuildExec)
+        .join(xbxr.RunSession)
+        .filter(xbxr.BuildExec.id == bx.id)
+        .filter(xbxro.CryptoAeadRun.mode == 'enc'))
+
+        # use pandas to make columns, then group into one table (dataframe)
+
+        ram = pd.Series([i.build_exec.build.bss +i.build_exec.build.data +i.stack_usage for i in q])
+        rom = pd.Series([i.build_exec.build.data +i.build_exec.build.text for i in q])
+
+        tp = pd.Series([i.measured_cycles/(i.plaintext_len + i.assoc_data_len) for i in q ])
+
+        enrgy = pd.Series([i.total_energy for i in q ])
+        avg_pwr = pd.Series([i.avg_power for i in q ])
+        max_pwr = pd.Series([i.max_power for i in q ])
+        length = pd.Series([i.plaintext_len+i.assoc_data_len for i in q ])
+
+        df = pd.DataFrame({'length':length,'ram':ram,'rom':rom,'tp':tp,'enrgy':enrgy,'avg_pwr':avg_pwr,'max_pwr':max_pwr}).sort_values(by='length')
+
+        print(df)
 
 
-    # q is filtered for everything in last runsession, only encryption results
-    q = s.query(xbxro.CryptoAeadRun).join(xbxr.BuildExec).join(xbxr.RunSession).filter(xbxr.RunSession.id == rs.id).filter(xbxro.CryptoAeadRun.mode == 'enc')
+        plt.suptitle(bx.build.implementation.path)
+        ax1 = plt.subplot(311)
+        ax1.set_ylabel("memory (B)")
+        ax1.set_xlabel("length (M+AD)")
+        ax1.plot(df['length'], df['ram'], '.-', color='red')
+        ax1.plot(df['length'], df['rom'], '.-',color='orange' )
+        ax1.legend(['ram', 'rom'])
+        ax1.set_ylim(bottom=0)
 
-    # use pandas to make columns, then group into one table (dataframe)
+        ax2_1 = plt.subplot(312)
+        ax2_1.plot(df['length'], df['avg_pwr'], '.-', color='yellow')
+        ax2_1.plot(df['length'], df['max_pwr'], '.-', color='green')
+        ax2_1.set_ylabel("power (W)")
+        ax2_1.set_xlabel("length (M+AD)")
 
-    ram = pd.Series([i.build_exec.build.bss +i.build_exec.build.data +i.stack_usage for i in q])
-    rom = pd.Series([i.build_exec.build.data +i.build_exec.build.text for i in q])
+        ax2_2 = ax2_1.twinx()
+        ax2_2.plot(df['length'],df['enrgy'], '.-', color="blue")
+        ax2_2.set_ylabel("energy (J)")
+        ax2_1.legend(['avg_pwr', 'max_pwr'], loc=6)
+        ax2_2.legend(['energy'], loc=7)
+        ax2_2.set_ylim(bottom=0)
+        ax2_1.set_ylim(bottom=0)
 
-    tp = pd.Series([i.measured_cycles/(i.plaintext_len + i.assoc_data_len) for i in q ])
-
-    enrgy = pd.Series([i.total_energy for i in q ])
-    avg_pwr = pd.Series([i.avg_power for i in q ])
-    max_pwr = pd.Series([i.max_power for i in q ])
-    length = pd.Series([i.plaintext_len+i.assoc_data_len for i in q ])
-
-    df = pd.DataFrame({'length':length,'ram':ram,'rom':rom,'tp':tp,'enrgy':enrgy,'avg_pwr':avg_pwr,'max_pwr':max_pwr}).sort_values(by='length')
-
-    print(df)
-
-
-
-    ax1 = plt.subplot(311)
-    ax1.set_ylabel("memory (B)")
-    ax1.set_xlabel("length (M+AD)")
-    ax1.plot(df['length'], df['ram'], '.-', color='red')
-    ax1.plot(df['length'], df['rom'], '.-',color='orange' )
-    ax1.legend(['ram', 'rom'])
-    ax1.set_ylim(bottom=0)
-
-    ax2_1 = plt.subplot(312)
-    ax2_1.plot(df['length'], df['avg_pwr'], '.-', color='yellow')
-    ax2_1.plot(df['length'], df['max_pwr'], '.-', color='green')
-    ax2_1.set_ylabel("power (W)")
-    ax2_1.set_xlabel("length (M+AD)")
-
-    ax2_2 = ax2_1.twinx()
-    ax2_2.plot(df['length'],df['enrgy'], '.-', color="blue")
-    ax2_2.set_ylabel("energy (J)")
-    ax2_1.legend(['avg_pwr', 'max_pwr'], loc=6)
-    ax2_2.legend(['energy'], loc=7)
-    ax2_2.set_ylim(bottom=0)
-    ax2_1.set_ylim(bottom=0)
-
-    ax3 = plt.subplot(313)
-    ax3.plot(df['length'],df['tp'], '.-')
-    ax3.set_xlabel("length (M+AD)")
-    ax3.set_ylabel("Throughput (Cycles/byte")
-    #ax3.set_yscale("log", nonposy='clip')
-    ax3.set_ylim(bottom=1)
-    #ax3.axis([0, 2000, 1, 10000])
-    plt.show()
+        ax3 = plt.subplot(313)
+        ax3.plot(df['length'],df['tp'], '.-')
+        ax3.set_xlabel("length (M+AD)")
+        ax3.set_ylabel("Throughput (Cycles/byte")
+        #ax3.set_yscale("log", nonposy='clip')
+        ax3.set_ylim(bottom=1)
+        #ax3.axis([0, 2000, 1, 10000])
+        plt.suptitle(impl)
+        plt.savefig("plots/"+str(impl).replace('/','_')+"_plot.pdf")
 
     
 
