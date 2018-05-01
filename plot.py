e03afbb7b3e34f068d3488b225d44cb2548f02d8
John Pham <jhnphm@gmail.com>
2018-05-01 04:59:29 -0400

    Unify plot_sorted and plot.py

--- plot.py
+++ plot.py
@@ -13,6 +13,7 @@ import xbx.util as xbxu
 import xbx.database as xbxdb
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
 CONFIG_PATH="config.ini"
 
@@ -24,50 +25,59 @@ def main():
     rs = s.query(xbxr.RunSession).order_by(xbxr.RunSession.id.desc()).first()
 
 
-    # q is filtered for everything in last runsession
+    # q is filtered for everything in last runsession, only encryption results
     q = s.query(xbxro.CryptoAeadRun).join(xbxr.BuildExec).join(xbxr.RunSession).filter(xbxr.RunSession.id == rs.id).filter(xbxro.CryptoAeadRun.mode == 'enc')
 
-    ram = [i.build_exec.build.bss +i.build_exec.build.data +i.stack_usage for i in q]
-    rom = [i.build_exec.build.data +i.build_exec.build.text for i in q]
+    # use pandas to make columns, then group into one table (dataframe)
 
-    tp = [i.measured_cycles/(i.plaintext_len + i.assoc_data_len) for i in q ]
+    ram = pd.Series([i.build_exec.build.bss +i.build_exec.build.data +i.stack_usage for i in q])
+    rom = pd.Series([i.build_exec.build.data +i.build_exec.build.text for i in q])
 
-    enrgy = [i.total_energy for i in q ]
-    avg_pwr = [i.avg_power for i in q ]
-    max_pwr = [i.max_power for i in q ]
+    tp = pd.Series([i.measured_cycles/(i.plaintext_len + i.assoc_data_len) for i in q ])
+
+    enrgy = pd.Series([i.total_energy for i in q ])
+    avg_pwr = pd.Series([i.avg_power for i in q ])
+    max_pwr = pd.Series([i.max_power for i in q ])
+    length = pd.Series([i.plaintext_len+i.assoc_data_len for i in q ])
+
+    df = pd.DataFrame({'length':length,'ram':ram,'rom':rom,'tp':tp,'enrgy':enrgy,'avg_pwr':avg_pwr,'max_pwr':max_pwr}).sort_values(by='length')
+
+    print(df)
 
 
-
-    length = [i.plaintext_len+i.assoc_data_len for i in q ]
 
     ax1 = plt.subplot(311)
     ax1.set_ylabel("memory (B)")
     ax1.set_xlabel("length (M+AD)")
-    ax1.plot(length, ram, '^', color='red')
-    ax1.plot(length,rom, 'o',color='orange' )
+    ax1.plot(df['length'], df['ram'], '.-', color='red')
+    ax1.plot(df['length'], df['rom'], '.-',color='orange' )
     ax1.legend(['ram', 'rom'])
+    ax1.set_ylim(bottom=0)
 
     ax2_1 = plt.subplot(312)
-    ax2_1.plot(length,avg_pwr, '^', color='yellow')
-    ax2_1.plot(length, max_pwr, 'o', color='green')
+    ax2_1.plot(df['length'], df['avg_pwr'], '.-', color='yellow')
+    ax2_1.plot(df['length'], df['max_pwr'], '.-', color='green')
     ax2_1.set_ylabel("power (W)")
     ax2_1.set_xlabel("length (M+AD)")
 
     ax2_2 = ax2_1.twinx()
-    ax2_2.plot(length,enrgy, 'b+')
+    ax2_2.plot(df['length'],df['enrgy'], '.-', color="blue")
     ax2_2.set_ylabel("energy (J)")
     ax2_1.legend(['avg_pwr', 'max_pwr'], loc=6)
     ax2_2.legend(['energy'], loc=7)
+    ax2_2.set_ylim(bottom=0)
+    ax2_1.set_ylim(bottom=0)
 
     ax3 = plt.subplot(313)
-    ax3.plot(length,tp, '^')
+    ax3.plot(df['length'],df['tp'], '.-')
     ax3.set_xlabel("length (M+AD)")
     ax3.set_ylabel("Throughput (Cycles/byte")
+    #ax3.set_yscale("log", nonposy='clip')
+    ax3.set_ylim(bottom=1)
+    #ax3.axis([0, 2000, 1, 10000])
     plt.show()
 
     
-    
-
 
 #    for i in q:
 #        print(i)
